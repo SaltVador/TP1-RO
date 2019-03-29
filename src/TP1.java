d9c9d15082774789431040d86b13f01cfb2ab149
SaltVador <36881066+SaltVador@users.noreply.github.com>
2019-03-29 12:12:33 +0100

    a* pas finis

--- src/TP1.java
+++ src/TP1.java
@@ -8,7 +8,7 @@ public class TP1 {
 
     private char[][] labyrinthe;
     //Coordonnées de la case de départ
-    private int startI, startJ;
+    private int startI, startJ,endI,endJ;
     private int longueur = 0;
 
     public TP1(){
@@ -31,6 +31,10 @@ public class TP1 {
                     if (var == 'E'){
                         this.startI=i;
                         this.startJ=j;
+                    }
+                    if (var == 'S'){
+                        this.endI=i;
+                        this.endJ=j;
                     }
                     j++;
                 }
@@ -229,9 +233,82 @@ public class TP1 {
         return 0;
     }
 
+    public Case laplusprochefin(ArrayList<Case> array){
+        Case retour = array.get(0);
+        int distT = endI-retour.getI()+endJ-retour.getJ();
+        if (distT<0)distT=-distT;
+        for (Case c : array) {
+            int test = endI-c.getI()+endJ-c.getJ();
+            if (test<0)test=-test;
+            if (test<distT){
+                distT=test;
+                retour=c;
+            }
+        }
+        return retour;
+    }
+
+    public int stratEtoile(){
+        Case grr = new Case(startI,startJ,0);
+        ArrayList<Case> atest = new ArrayList<Case>();
+        ArrayList<Case> frontiere = new ArrayList<Case>();
+        atest.add(grr);
+        while(!atest.isEmpty()){
+            Case test = laplusprochefin(atest);
+            if (test.getI()!=0){
+                Case enPlusH = new Case(test.getI()-1,test.getJ(),test.getLongueur()+1);
+                if (enHaut(enPlusH,frontiere)) {
+                    System.out.println("haut i = "+enPlusH.getI()+"  j = "+enPlusH.getJ());
+                    atest.add(enPlusH);
+                    frontiere.add(enPlusH);
+                    if (estFin(enPlusH.getI(),enPlusH.getJ())){
+                        return enPlusH.getLongueur();
+                    }
+                }}
+
+            if (test.getI()!=labyrinthe[0].length){
+                Case enPlusB = new Case(test.getI()+1,test.getJ(),test.getLongueur()+1);
+                if (enBas(enPlusB,frontiere)) {
+                    System.out.println("bas i = "+enPlusB.getI()+"  j = "+enPlusB.getJ());
+                    atest.add(enPlusB);
+                    frontiere.add(enPlusB);
+                    if (estFin(enPlusB.getI(),enPlusB.getJ())){
+                        return enPlusB.getLongueur();
+                    }
+                }
+            }
+
+            if (test.getJ() != labyrinthe.length){
+                Case enPlusD = new Case(test.getI(),test.getJ()+1,test.getLongueur()+1);
+                if (aDroite(enPlusD,frontiere)) {
+                    System.out.println("droite i = "+enPlusD.getI()+"  j = "+enPlusD.getJ());
+                    atest.add(enPlusD);
+                    frontiere.add(enPlusD);
+                    if (estFin(enPlusD.getI(),enPlusD.getJ())){
+                        return enPlusD.getLongueur();
+                    }
+                }
+            }
+
+            if (test.getJ()!=0){
+                Case enPlusG = new Case(test.getI(),test.getJ()-1,test.getLongueur()+1);
+                if (aGauche(enPlusG,frontiere)) {
+                    System.out.println("gauche i = "+enPlusG.getI()+"  j = "+enPlusG.getJ());
+                    atest.add(enPlusG);
+                    frontiere.add(enPlusG);
+                    if (estFin(enPlusG.getI(),enPlusG.getJ())){
+                        return enPlusG.getLongueur();
+                    }
+                }
+            }
+            atest.remove(test);
+        }
+        return 0;
+    }
+
     public static void main (String[] agrs){
         TP1 lab = new TP1();
-        System.out.println(lab.enProfondeur());
+        System.out.println(lab.stratEtoile());
     }
 
 }
